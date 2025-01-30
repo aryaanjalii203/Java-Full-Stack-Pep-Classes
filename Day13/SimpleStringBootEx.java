eabd4e5693cb4209a6f9a6874ca2d82d15741d1a
Anjali Kumari <120202034+aryaanjalii203@users.noreply.github.com>
2025-01-30 14:58:39 +0530

    Add files via upload

---
+++ Day13/SimpleStringBootEx.java
@@ -0,0 +1,14 @@
+package com.example.SecondProjectSTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondProjectStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondProjectStsApplication.class, args);
+		System.out.println("hello guys");
+	}
+
+}
